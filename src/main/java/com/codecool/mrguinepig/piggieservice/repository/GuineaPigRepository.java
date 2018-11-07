f8f86ebf856e48ace16b95a6edaaaff8b95c2421
Balint Mogyorosi <mogyorosibalin@gmail.com>
2018-11-07 10:47:07 +0100

    removed comment

--- src/main/java/com/codecool/mrguinepig/piggieservice/repository/GuineaPigRepository.java
+++ src/main/java/com/codecool/mrguinepig/piggieservice/repository/GuineaPigRepository.java
@@ -4,13 +4,10 @@ import com.codecool.mrguinepig.piggieservice.model.GuineaPig;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GuineaPigRepository extends JpaRepository<GuineaPig, Long> {
-
-    // Optional<GuineaPig> findFirstByIdOrderByIdDesc();
 
     Optional<GuineaPig> findTopByIdIsNotNullOrderByIdDesc();
 
